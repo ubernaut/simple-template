c9eda95ec17512df0c8a04475abfffd96ecfbac4
cos <collin.schroeder@gmail.com>
2024-07-08 14:53:09 -0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+build/run local:
+npm start
+publish:
+npm run build-gh-pages
+git add .dist
+git commit -m "deploy whatever"
+git subtree push --prefix dist origin gh-pages
---
+++ js/app.js
@@ -0,0 +1,66 @@
+import * as THREE from "three";
+
+// window.addEventListener("load", () => {
+//   registerSW();
+// });
+
+//fire up three.js
+
+let camera, scene, renderer;
+let mesh;
+
+init();
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    90,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    100,
+  );
+  camera.position.z = 2;
+
+  scene = new THREE.Scene();
+
+  geometry = new THREE.BoxGeometry(1, 1, 1);
+  material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animate);
+  document.body.appendChild(renderer.domElement);
+
+  //
+
+  window.addEventListener("resize", onWindowResize);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+  cube.rotation.x += 0.005;
+  cube.rotation.y += 0.01;
+
+  renderer.render(scene, camera);
+}
+
+// // Register the Service Worker
+// async function registerSW() {
+//   if ("serviceWorker" in navigator) {
+//     try {
+//       const sw = navigator.serviceWorker;
+//       await sw.register("js/serviceworker.js");
+//     } catch (e) {
+//       console.log("SW registration failed");
+//       console.log(e);
+//     }
+//   }
+// }
